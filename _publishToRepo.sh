666f8ae8fcfa7e077dd97618307a6a1cc48c19e1
JC Holder <jc.holder@thoughtworks.com>
2021-02-01 16:18:28 -0500

    Package up json+elm (?) in published package alongside bundled json

--- _publishToRepo.sh
+++ _publishToRepo.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 tar -zxf ./output/package.tgz -C ./output
 node updatePackage.js
-cd ./output/package
+cd ./output/
+cp *-2.json package/ # Copy over original json+eml
+cd ./package
 echo "registry=https://npm.pkg.github.com/chronic-care" >> .npmrc
 npm publish
