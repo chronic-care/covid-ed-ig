fc3e64e4b0e7a1120bfff692437abb3f5a8ad465
JC Holder <jc.holder@thoughtworks.com>
2021-02-01 17:08:49 -0500

    Refactor _publishToRepo to minimize directory changes

--- _publishToRepo.sh
+++ _publishToRepo.sh
@@ -2,7 +2,7 @@
 tar -zxf ./output/package.tgz -C ./output
 node updatePackage.js
 cd ./output/
-cp *-2.json package/ # Copy over original json+eml
-cd ./package
-echo "registry=https://npm.pkg.github.com/chronic-care" >> .npmrc
+cp ./output/*-2.json ./output/package/ # Copy over original json+eml
+echo "registry=https://npm.pkg.github.com/chronic-care" >> ./output/package/.npmrc
+cd ./output/package
 npm publish
