5bcc2712a8c99469358edd6aad9e106b861eb608
JC Holder <jc.holder@thoughtworks.com>
2021-02-03 11:51:08 -0500

    Prototype on-tagging-for-release workflow

--- _publishToRepo.sh
+++ _publishToRepo.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-tar -zxf ./output/package.tgz -C ./output
-node updatePackage.js
-cp ./output/*-2.json ./output/package/ # Copy over original json+eml
-node localizeLibraryPaths.js ./output/package/*-2.json
-echo "registry=https://npm.pkg.github.com/chronic-care" >> ./output/package/.npmrc
-cd ./output/package
-npm publish
+
+packageVersion=false
+
+while [ "$#" -gt 0 ]; do
+    case $1 in
+    --release-version) packageVersion="$2" ;;
+    esac
+    shift
+done
+
+echo Package Version: $packageVersion
+
+#tar -zxf ./output/package.tgz -C ./output
+#node updatePackage.js
+#cp ./output/*-2.json ./output/package/ # Copy over original json+eml
+#node localizeLibraryPaths.js ./output/package/*-2.json
+#echo "registry=https://npm.pkg.github.com/chronic-care" >> ./output/package/.npmrc
+#cd ./output/package
+#npm publish
