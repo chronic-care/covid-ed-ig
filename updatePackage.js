5362722f39a32143bfe6de03d976d175116dcc67
JC Holder <jc.holder@thoughtworks.com>
2021-02-03 13:44:20 -0500

    Incorporate tagging fully into package and publication workflow

--- updatePackage.js
+++ updatePackage.js
@@ -1,4 +1,22 @@
 const jsonfile = require('jsonfile')
+const argv = require('minimist')(process.argv.slice(2))
+
+const releaseTag = argv['release-tag']
+
+if (!releaseTag) {
+    console.log('Missing argument: --release-tag <tag>')
+    process.exit(1)
+}
+
+const tagMatch = releaseTag.match(/v(\d+\.\d+\.\d+)$/)
+
+if (!tagMatch) {
+    console.log(`Invalid release tag format.\nReceived "${releaseTag} but expected it to end with "vX.Y.Z" where X.Y.Z is the semver of the release, e.g. v0.0.1`)
+    process.exit(1)
+}
+
+const packageTag = tagMatch[1]
+console.log(`Packaging and publishing release ${packageTag}`)
 
 const packageJsonPath = 'output/package/package.json'
 const libraryPackageJsonPath = 'library-package.json'
