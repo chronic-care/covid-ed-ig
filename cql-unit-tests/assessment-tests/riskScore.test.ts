b970d41a7ce9c60cde2ef31eb687dc6ea48ed220
Marco Barragan <mbarraga@thoughtworks.com>
2021-04-15 12:16:09 -0400

    Test Respiratory Rate Risk Score
    Co-authored-by: Marco Barragan <mbarraga@thoughtworks.com>
    Co-authored-by: Marisa Hoenig <marisa.hoenig@thoughtworks.com>

--- cql-unit-tests/assessment-tests/riskScore.test.ts
+++ cql-unit-tests/assessment-tests/riskScore.test.ts
@@ -3,6 +3,33 @@ import { executeAssessmentCQLExpression } from '../helpers/cqlService';
 
 describe('risk score with parameter overrides', () => {
     test.each`
+        respiratoryRate | expectedScore
+        ${12}           | ${0}
+        ${20}           | ${0}
+        ${null}         | ${0}
+        ${9}            | ${1}
+        ${11}           | ${1}
+        ${21}           | ${2}
+        ${24}           | ${2}
+        ${8}            | ${3}
+        ${25}           | ${3}
+    `('returns score of $expectedScore for respiratory rate value of $respiratoryRate', ({
+        respiratoryRate, expectedScore
+    }) => {
+
+        const cqlExpressionParameters = {
+            IgnoreFallbackResourceValues: true,
+            PatientData: undefined,
+            RiskFactors: undefined,
+            ClinicalAssessments: buildDefaultClinicalAssessmentParameters({RespiratoryRate: respiratoryRate}),
+        };
+
+        const respiratoryRateScore = executeAssessmentCQLExpression(cqlExpressionParameters, 'Respiratory Rate Risk Score');
+
+        expect(respiratoryRateScore).toEqual(expectedScore);
+    });
+
+    test.each`
         heartRate | expectedScore
         ${51}     | ${0}
         ${90}     | ${0}
@@ -27,7 +54,6 @@ describe('risk score with parameter overrides', () => {
         };
 
         const heartRateScore = executeAssessmentCQLExpression(cqlExpressionParameters, 'Heart Rate Risk Score');
-
 
         expect(heartRateScore).toEqual(expectedScore);
     });
