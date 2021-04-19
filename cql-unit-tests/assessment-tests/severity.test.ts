0500cec61c3e17ce9fe438ccd1c80fe113577876
Marco Barragan <mbarraga@thoughtworks.com>
2021-04-19 10:50:03 -0400

    Create tests for Severe Severity Classification

--- cql-unit-tests/assessment-tests/severity.test.ts
+++ cql-unit-tests/assessment-tests/severity.test.ts
@@ -30,4 +30,34 @@ describe('severity with parameter overrides', () => {
 
         expect(isCriticalSeverity).toEqual(expectedCriticalSeverity);
     });
+
+    test.each`
+        o2Saturation   | respiratoryRate | paO2FiO2 | lungInfiltrates | expectedSevereSeverity
+        ${93}             | ${null}      | ${null}         | ${null}  |  ${true}
+        ${94}             | ${null}      | ${null}         | ${null}  |  ${false}
+        ${null}            | ${35}       | ${null}         | ${null}  | ${true}
+        ${null}            | ${30}       | ${null}         | ${null}  | ${false}
+        ${null}            | ${null}      | ${300}          | ${null} | ${false}
+        ${null}            | ${null}      | ${299}          | ${null} | ${true}
+        ${null}            | ${null}      | ${null}         | ${true} | ${true}
+    `(`returns expectedSevereSeverity=$expectedSevereSeverity when values are ,
+    o2Saturation=$o2Saturation, respiratoryRate=$respiratoryRate, paO2FiO2=$paO2FiO2, lungInfiltrates=$lungInfiltrates`, ({
+        o2Saturation, respiratoryRate, paO2FiO2, lungInfiltrates, expectedSevereSeverity
+    }) => {
+        const cqlExpressionParameters = {
+            IgnoreFallbackResourceValues: true,
+            PatientData: undefined,
+            RiskFactors: undefined,
+            ClinicalAssessments: buildDefaultClinicalAssessmentParameters({
+                O2Saturation: o2Saturation,
+                RespiratoryRate: respiratoryRate,
+                PaO2FiO2Ratio: paO2FiO2,
+                LungInfiltratesOver50: lungInfiltrates
+            }),
+        };
+
+        const isSevereSeverity = executeAssessmentCQLExpression(cqlExpressionParameters, 'Is Severe Severity');
+
+        expect(isSevereSeverity).toEqual(expectedSevereSeverity);
+    });
 });
