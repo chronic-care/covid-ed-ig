c390c5305bccce172c2fc5342c4284ecec8cc438
Ellis Crawford <ellis.crawford@thoughtworks.com>
2021-04-19 16:25:36 -0400

    Create test for ConcerningImagingCount
    Co-authored-by: Marco Barragan <mbarraga@thoughtworks.com>
    Co-authored-by: Ellis Crawford <ellis.crawford@thoughtworks.com>

--- cql-unit-tests/assessment-tests/diagnosticInterpretation.test.ts
+++ cql-unit-tests/assessment-tests/diagnosticInterpretation.test.ts
@@ -23,4 +23,30 @@ describe('diagnostic interpretation',() => {
         expect(CQLConcerningLabCount).toEqual(expectedConcerningLabCount);
     });
 
+    test.each`
+        chestXRayConcerning | ultrasoundConcerning | cTConcerning | expectedConcerningImagingCount
+        ${true}             | ${false}             | ${false}     | ${1}
+        ${true}             | ${true}              | ${false}     | ${2}
+        ${true}             | ${true}              | ${true}      | ${3}
+        ${true}             | ${false}             | ${true}      | ${2}
+        ${false}            | ${false}             | ${true}      | ${1}
+     
+    `(`returns expectedConcerningImagingCount=$expectedConcerningImagingCount when values 
+    are chestXRayConcerning=$chestXRayConcerning, ultrasoundConcerning=$ultrasoundConcerning, 
+     cTConcerning=$cTConcerning`, ({ chestXRayConcerning, ultrasoundConcerning, cTConcerning, expectedConcerningImagingCount }) => {
+        const cqlExpressionParameters = {
+            IgnoreFallbackResourceValues: true,
+            PatientData: undefined,
+            RiskFactors: undefined,
+            ClinicalAssessments: buildDefaultClinicalAssessmentParameters({
+                ChestXRayConcerning: chestXRayConcerning,
+                UltrasoundConcerning: ultrasoundConcerning,
+                CTConcerning: cTConcerning
+            }),
+        };
+
+        const CQLConcerningImagingCount = executeSummaryCQLExpression(cqlExpressionParameters, 'ConcerningImagingCount');
+
+        expect(CQLConcerningImagingCount).toEqual(expectedConcerningImagingCount);
+    });
 });
