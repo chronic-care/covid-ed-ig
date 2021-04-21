428b291f82c3c07c05a4876a29c7ce88e8829a97
Ellis Crawford <ellis.crawford@thoughtworks.com>
2021-04-21 15:53:27 -0400

    Create test for diagnostic summary for LDH lab result
    Co-authored-by: Ellis Crawford <ellis.crawford@thoughtworks.com>
    Co-authored-by: Marco Barragan <mbarraga@thoughtworks.com>

--- cql-unit-tests/test/diagnosticSummary.test.ts
+++ cql-unit-tests/test/diagnosticSummary.test.ts
@@ -423,4 +423,67 @@ describe('diagnostic interpretation with fhir resources', () => {
         expect(Date.parse(retrievedFerritin!.Date)).toEqual(Date.parse(expectedLabResult.Date));
     });
 
+    test('obtains latest LDH Lab Result in Diagnostic Summary Section', () => {
+
+        const oldestSeconds = 10;
+
+        const oldestDate = new Date(2020, 10, 10, 1, 30, oldestSeconds);
+        const middleDate = new Date(2020, 10, 10, 1, 30, oldestSeconds + 10);
+        const newestDate = new Date(2020, 10, 10, 1, 30, oldestSeconds + 20);
+        const coding = [
+            {
+                "system": "http://loinc.org",
+                "display" : "LDH",
+                "code": "2532-0"
+            }
+        ]
+
+        const oldestLabResult: Resource = new LabResultBuilder()
+            .withId('oldie')
+            .withCoding(coding)
+            .withEffectiveDateTime(oldestDate.toISOString())
+            .build();
+
+        const middleLabResult: Resource = new LabResultBuilder()
+            .withId('middle')
+            .withCoding(coding)
+            .withEffectiveDateTime(middleDate.toISOString())
+            .build();
+
+        const newestLabResult: Resource = new LabResultBuilder()
+            .withId('newer')
+            .withCoding(coding)
+            .withEffectiveDateTime(newestDate.toISOString())
+            .build();
+
+        const expectedLabResult: Diagnostic = {
+            Date: newestDate.toISOString(),
+            Flag: false,
+            Interpretation: null,
+            Name: newestLabResult.resource.code.coding![0].display!,
+            ReferenceRange: "",
+            ResultText: `${newestLabResult.resource.valueQuantity!.value} ${newestLabResult.resource.valueQuantity!.unit}`,
+            ResultUnits: newestLabResult.resource.valueQuantity!.unit!,
+            ResultValue: newestLabResult.resource.valueQuantity!.value!
+        }
+
+        const diagnosticSummary: DiagnosticSummary = executeSummaryNoParams('DiagnosticSummary', [
+                oldestLabResult,
+                middleLabResult,
+                newestLabResult,
+            ]
+        ) as DiagnosticSummary;
+
+
+        const retrievedLDH = diagnosticSummary.LDH;
+
+        expect(retrievedLDH).not.toBeNull();
+        expect(retrievedLDH!.Flag).toEqual(expectedLabResult.Flag);
+        expect(retrievedLDH!.Interpretation).toEqual(expectedLabResult.Interpretation);
+        expect(retrievedLDH!.Name).toEqual(expectedLabResult.Name);
+        expect(retrievedLDH!.ResultText).toEqual(expectedLabResult.ResultText);
+        expect(Date.parse(retrievedLDH!.Date)).toEqual(Date.parse(expectedLabResult.Date));
+    });
+
+
 });
