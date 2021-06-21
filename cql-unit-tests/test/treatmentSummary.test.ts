e889034a023facbbf7f8e87300dc6021bfd712a9
Marisa Hoenig <marisa.hoenig@gmail.com>
2021-06-21 14:45:28 -0600

    Update Recommend Remdesivir Treatment logic to match May ACEP changes
    Co-authored-by: Ellis Crawford <ellis.crawford@thoughtworks.com>
    Co-authored-by: Marisa Hoenig <marisa.hoenig@thoughtworks.com>

--- cql-unit-tests/test/treatmentSummary.test.ts
+++ cql-unit-tests/test/treatmentSummary.test.ts
@@ -92,7 +92,7 @@ describe('treatment summary', () => {
 
     it.each([
             ['mild', 'insufficient-evidence', new ClinicalAssessmentBuilder().withMildSeverity().build()],
-            ['moderate', 'use', new ClinicalAssessmentBuilder().withModerateSeverity().withConcerningLab(1).build()],
+            ['moderate', 'insufficient-evidence', new ClinicalAssessmentBuilder().withModerateSeverity().withConcerningLab(1).build()],
             ['severe', null, new ClinicalAssessmentBuilder().withSevereSeverity().build()],
             ['critical', null, new ClinicalAssessmentBuilder().withCriticalSeverity().build()],
             ['none', null, {}],
