6d862f2161e85a9560c6bcff2b6f440c7e07ba2e
JC Holder <jc.holder@thoughtworks.com>
2021-07-15 14:08:45 -0400

    Extract testValueOverrideViaParameter. Refactor "overriding original values with parameters" tests to accommodate the full range of conditions that will be tested
    Co-authored-by: Rebecca Green <rebecca.green@thoughtworks.com>
    Co-authored-by: Marisa Hoenig <marisa.hoenig@thoughtworks.com>
    Co-authored-by: JC Holder <jc.holder@thoughtworks.com>

--- cql-unit-tests/test/riskFactorsCount.test.ts
+++ cql-unit-tests/test/riskFactorsCount.test.ts
@@ -7,6 +7,42 @@ import { executeAssessmentCQLExpression } from "../helpers/cqlService";
 import { ICondition } from "@ahryman40k/ts-fhir-types/lib/R4";
 import CancerBuilder from "./builders/Condition/CancerBuilder";
 
+const testValueOverrideViaParameter = (hasCondition: boolean,
+                                       conditionType: string,
+                                       cqlExpression: string,
+                                       cqlParameter: boolean | null,
+                                       expected: boolean | null,
+                                       builder: any) => {
+    const riskAssessmentScoreParameters: RiskAssessmentScoreParameters = {
+        Cancer: null,
+        CardiovascularDisease: null,
+        ChronicRespiratoryDisease: null,
+        DiabetesType2: null,
+        DownsSyndrome: null,
+        Hypertension: null,
+        Immunosuppression: null,
+        NeurologicDisease: null,
+        Obesity: null,
+        ObstructiveSleepApnea: null,
+        Pregnancy: null,
+        RenalDisease: null,
+        SteroidUsage: null,
+        [conditionType]: cqlParameter,
+    };
+
+    const conditions: ICondition[] = hasCondition ? [ new builder().build() ] : [];
+
+    const cqlParams: CQLExpressionParameters = {
+        ClinicalAssessments: null,
+        IgnoreFallbackResourceValues: true,
+        PatientData: null,
+        RiskFactors: riskAssessmentScoreParameters,
+    };
+
+    const results = executeAssessmentCQLExpression(cqlParams, cqlExpression, conditions);
+    expect(results).toEqual(expected);
+};
+
 describe('risk assessment score', () => {
     it('returns 0 when all inputs are null', () => {
         const riskAssessmentScoreParameters = buildAllNullRiskAssessmentScoreParameters();
@@ -68,66 +104,27 @@ describe('risk assessment score', () => {
     });
 
     describe('overriding original values with parameters', () => {
-        test.each`
-            hasCondition | conditionType                     | cqlExpression                        | cqlParameter | expected  | builder
-            ${`has`}     | ${`Cancer`}                       | ${`Has Cancer Risk Factor`}          | ${true}      | ${true}   | ${CancerBuilder}
-            ${`has`}     | ${`Cancer`}                       | ${`Has Cancer Risk Factor`}          | ${false}     | ${false}  | ${CancerBuilder}
-            ${`has`}     | ${`Cancer`}                       | ${`Has Cancer Risk Factor`}          | ${null}      | ${null}   | ${CancerBuilder}
-            ${`lacks`}   | ${`Cancer`}                       | ${`Has Cancer Risk Factor`}          | ${true}      | ${true}   | ${CancerBuilder}
-            ${`lacks`}   | ${`Cancer`}                       | ${`Has Cancer Risk Factor`}          | ${false}     | ${null}   | ${CancerBuilder}
-            ${`lacks`}   | ${`Cancer`}                       | ${`Has Cancer Risk Factor`}          | ${null}      | ${null}   | ${CancerBuilder}
-        `('When the patient $hasCondition at least one condition of type $conditionType and that risk factor parameter is $cqlParameter then the calculated risk factor is $expected',
-        ({ hasCondition, conditionType, cqlExpression, cqlParameter, expected, builder }) => {
-            const riskAssessmentScoreParameters: RiskAssessmentScoreParameters = {
-                Cancer: null,
-                CardiovascularDisease: null,
-                ChronicRespiratoryDisease: null,
-                DiabetesType2: null,
-                DownsSyndrome: null,
-                Hypertension: null,
-                Immunosuppression: null,
-                NeurologicDisease: null,
-                Obesity: null,
-                ObstructiveSleepApnea: null,
-                Pregnancy: null,
-                RenalDisease: null,
-                SteroidUsage: null,
-                [conditionType]: cqlParameter,
-            };
-
-            const conditions: ICondition[] = hasCondition === 'has' ? [ new builder().build() ] : [];
-
-            const cqlParams: CQLExpressionParameters = {
-                ClinicalAssessments: null,
-                IgnoreFallbackResourceValues: true,
-                PatientData: null,
-                RiskFactors: riskAssessmentScoreParameters,
-            };
-
-            const results = executeAssessmentCQLExpression(cqlParams, cqlExpression, conditions);
-            expect(results).toEqual(expected);
+        [
+            { conditionType: 'Cancer', cqlExpression: 'Has Cancer Risk Factor', builder: CancerBuilder },
+        ].forEach(({conditionType, cqlExpression, builder}) => {
+            test(`Given that the patient has at least one condition of type ${conditionType} and the risk factor parameter is true then the calculated risk factor is true`, () => {
+                testValueOverrideViaParameter(true, conditionType, cqlExpression, true, true, builder);
+            });
+            test(`Given that the patient has at least one condition of type ${conditionType} and the risk factor parameter is false then the calculated risk factor is false`, () => {
+                testValueOverrideViaParameter(true, conditionType, cqlExpression, true, true, builder);
+            });
+            test(`Given that the patient has at least one condition of type ${conditionType} and the risk factor parameter is null then the calculated risk factor is null`, () => {
+                testValueOverrideViaParameter(true, conditionType, cqlExpression, true, true, builder);
+            });
+            test(`Given that the patient does not have any condition of type ${conditionType} and the risk factor parameter is true then the calculated risk factor is true`, () => {
+                testValueOverrideViaParameter(true, conditionType, cqlExpression, true, true, builder);
+            });
+            test(`Given that the patient does not have any condition of type ${conditionType} and the risk factor parameter is false then the calculated risk factor is false`, () => {
+                testValueOverrideViaParameter(true, conditionType, cqlExpression, true, true, builder);
+            });
+            test(`Given that the patient does not have any condition of type ${conditionType} and the risk factor parameter is null then the calculated risk factor is null`, () => {
+                testValueOverrideViaParameter(true, conditionType, cqlExpression, true, true, builder);
+            });
         });
-    });
-
-    describe('Cancer', () => {
-       describe('given that the patient has at least one condition with a code for the given condition type', () => {
-           // TODO: Still necessary?
-           describe('when initially calculating their risk factor count', () => {
-               test('then the calculated risk factor should be true', () => {
-
-                   const conditions: ICondition[] = [
-                        new CancerBuilder().build()
-                   ]
-                   const cqlParams: CQLExpressionParameters = {
-                       ClinicalAssessments: null,
-                       IgnoreFallbackResourceValues: false,
-                       PatientData: null,
-                       RiskFactors: null,
-                   };
-                   const results = executeAssessmentCQLExpression(cqlParams, 'Has Cancer Risk Factor', conditions);
-                   expect(results).toEqual(true);
-               });
-           });
-       });
     });
 });
